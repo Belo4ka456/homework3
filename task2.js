a09c88a454b19d6a7bbfc93ee6bb7368cb1fcd15
Belo4ka456 <morozova.julia.v@gmail.com>
2022-03-23 23:45:18 +0100

    arr

---
+++ task2.js
@@ -0,0 +1,7 @@
+let fruits=["Apple","Orange"]
+
+fruits.push=("Banana") //добавили єлемент Banana
+fruits[1]="Mango"// заменили Оранж на Манго
+fruits.shift(0)// извлекли первій єлемент
+fruits.unshift("Pear","Melon")//добавили в начало 2 новіх єлемента
+console.log(fruits)
---
+++ task3.js
@@ -0,0 +1,5 @@
+let arr=["o","f","a","n","g"]
+arr[1]="r"// замена букві 
+arr.push("e")// добавили букву в конец массива
+console.log(arr.join(''))// вівели слово orange
+//console.log(arr)
